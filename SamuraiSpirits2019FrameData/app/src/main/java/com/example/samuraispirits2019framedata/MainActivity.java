01ba8c6824f0c7d87ba0cf177652687822e60df7
konasuke <kurokabuto0126@gmail.com>
2019-09-23 01:29:13 +0900

    SamuraiSpirits2019FrameData

---
+++ SamuraiSpirits2019FrameData/app/src/main/java/com/example/samuraispirits2019framedata/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.samuraispirits2019framedata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // インテントオブジェクトを取得
+        Intent intent  =getIntent();
+        // ListViewオブジェクトを取得
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        // ListViewにリスナを設定
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    /**
+     * リストがタップされたときの処理が記述されたメンバクラス。
+     */
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // タップされた定食名を取得
+            String item = (String) parent.getItemAtPosition(position);
+            // トーストで表示する文字列を生成SplashActivity
+            String show = "あなたが選んだ定食: " + item;
+            // トーストの表示
+            Toast.makeText(MainActivity.this, show, Toast.LENGTH_LONG).show();
+        }
+    }
+}
