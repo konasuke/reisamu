7186eea7b2f81cf540810f1fd2ddccf821543c24
yasu <yasu5614@gmail.com>
2019-11-09 16:18:01 +0900

    ListViewイメージ化

--- SamuraiSpirits2019_Capture/app/src/main/java/android/wings/websarva/samuraispirits2019_capture/MainActivity.java
+++ SamuraiSpirits2019_Capture/app/src/main/java/android/wings/websarva/samuraispirits2019_capture/MainActivity.java
@@ -1,16 +1,43 @@
 package android.wings.websarva.samuraispirits2019_capture;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private static final String[] names = {
+            "chara01",
+            "chara02",
+            "chara03",
+            "chara04",
+            "chara05",
+            "chara06",
+            "chara07",
+            "chara08",
+            "chara09",
+            "chara10"
+    };
+
+    // drawableに画像を入れる、R.id.xxx はint型
+    private static final int[] photos = {
+            R.drawable.chara01,
+            R.drawable.chara02,
+            R.drawable.chara03,
+            R.drawable.chara04,
+            R.drawable.chara05,
+            R.drawable.chara06,
+            R.drawable.chara07,
+            R.drawable.chara08,
+            R.drawable.chara09,
+            R.drawable.chara10
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +47,13 @@ public class MainActivity extends Activity {
         //インテントオブジェクトを取得。
         Intent intent = getIntent();
         //ListViewオブジェクトを取得。
-        ListView lvMenu = findViewById(R.id.lvMenu);
-        //ListViewにリスナを設定。
-        lvMenu.setOnItemClickListener(new ListItemClickListener());
+        ListView listView = findViewById(R.id.listView);
+
+        BaseAdapter adapter = new TestAdapter(this.getApplicationContext(),
+                R.layout.list_items, photos);
+
+        // ListViewにadapterをセット
+        listView.setAdapter(adapter);
     }
     /**
      * リストがタップされたときの処理が記述されたメンバクラス。
