7fc3c2d34f7d49f0e9ce6b1edeae9701f9353b4b
konasuke <kurokabuto0126@gmail.com>
2019-11-10 03:21:12 +0900

    キャラ詳細画面、マニフェスト修正

--- SamuraiSpirits2019_Capture/app/src/main/java/android/wings/websarva/samuraispirits2019_capture/CharacterIntroduction.java
+++ SamuraiSpirits2019_Capture/app/src/main/java/android/wings/websarva/samuraispirits2019_capture/CharacterIntroduction.java
@@ -3,6 +3,7 @@ package android.wings.websarva.samuraispirits2019_capture;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,13 +13,23 @@ public class CharacterIntroduction extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.character_introduction);
 
-        Intent intent = getIntent();
-        String selectedText = intent.getStringExtra("Text");
-        int selectedPhoto = intent.getIntExtra("Photo", 0);
+//        Intent intent = getIntent();
+//        String selectedText = intent.getStringExtra("Text");
+//        int selectedPhoto = intent.getIntExtra("Photo", 0);
+//
+//        TextView textView = findViewById(R.id.selected_text);
+//        textView.setText(selectedText);
+//        ImageView  imageView = findViewById(R.id.selected_photo);
+//        imageView.setImageResource(selectedPhoto);
 
-        TextView textView = findViewById(R.id.selected_text);
-        textView.setText(selectedText);
-        ImageView  imageView = findViewById(R.id.selected_photo);
-        imageView.setImageResource(selectedPhoto);
+        Button commandListButton = findViewById(R.id.commandListButton);
+        commandListButton.setText("コマンドリスト");
+
+        Button memoButton = findViewById(R.id.memoButton);
+        memoButton.setText("メモ");
+        memoButton.setEnabled(false);
+
+        Button backButton = findViewById(R.id.backButton);
+        backButton.setText("戻る");
     }
 }
