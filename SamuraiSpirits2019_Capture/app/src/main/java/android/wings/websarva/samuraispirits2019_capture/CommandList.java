f9ca6d54ace0bfe8d06293ce496eafc85b4a6a76
konasuke <kurokabuto0126@gmail.com>
2019-11-16 11:51:46 +0900

    add CommandList

---
+++ SamuraiSpirits2019_Capture/app/src/main/java/android/wings/websarva/samuraispirits2019_capture/CommandList.java
@@ -0,0 +1,15 @@
+package android.wings.websarva.samuraispirits2019_capture;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CommandList  extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.command_list);
+
+    }
+}
