c73943d924bc304a2c8900a96dc2d9128445aca3
yasu <yasu5614@gmail.com>
2019-11-16 11:55:26 +0900

    余分なインポートを削除

--- SamuraiSpirits2019_Capture/app/src/main/java/android/wings/websarva/samuraispirits2019_capture/SplashActivity.java
+++ SamuraiSpirits2019_Capture/app/src/main/java/android/wings/websarva/samuraispirits2019_capture/SplashActivity.java
@@ -1,12 +1,10 @@
 package android.wings.websarva.samuraispirits2019_capture;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Window;
+
 
 public class SplashActivity extends Activity {
 
