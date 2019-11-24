981cc60a259122048e4091fd26ae2e9b5e458c95
yasu <yasu5614@gmail.com>
2019-11-24 15:25:45 +0900

    キャラセレ画面 出力リストをjsonから読み込む様に修正 ※画像の動的に読み込む方法がうまくいかないののののんおおおぽおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおおお

--- SamuraiSpirits2019_Capture/app/src/main/java/android/wings/websarva/samuraispirits2019_capture/MainActivity.java
+++ SamuraiSpirits2019_Capture/app/src/main/java/android/wings/websarva/samuraispirits2019_capture/MainActivity.java
@@ -8,23 +8,46 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+
 public class MainActivity extends Activity implements AdapterView.OnItemClickListener {
 
-    private static final String[] scenes  = {
-            "HAOHMARU",
-            "NAKORURU",
-            "HATTORIHANZO",
-            "GALFORD",
-            "TACHIBANAUKYO",
-            "SENRYOKYOSHIRO",
-            "YAGYUJUBEI",
-            "TAMTAM",
-            "CHARLOTTE",
-            "KIBAGAMI_GENJURO"
-    };
+    private String[] scenes;
+    /**
+     * フィールドの値を取得します。
+     */
+    public String[] getScenes()
+    {
+        return scenes;
+    }
+
+    /**
+     * フィールドに値を入れます。
+     */
+    public void setScenes( String[] param )
+    {
+        scenes = param;
+    }
+private final String CHARA_NAME = "CHARA_NAME";
+
+//    private static final String[] scenes  = {
+//            "HAOHMARU",
+//            "NAKORURU",
+//            "HATTORIHANZO",
+//            "GALFORD",
+//            "TACHIBANAUKYO",
+//            "SENRYOKYOSHIRO",
+//            "YAGYUJUBEI",
+//            "TAMTAM",
+//            "CHARLOTTE",
+//            "KIBAGAMI_GENJURO"
+//    };
 
     // drawableに画像を入れる、R.id.xxx はint型
-    private static final int[] photos = {
+    private static final int[] images = {
             R.drawable.chara01,
             R.drawable.chara02,
             R.drawable.chara03,
@@ -42,6 +65,8 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        createCharaList();
+
         // ListViewのインスタンスを生成
         ListView listView = findViewById(R.id.listview);
 
@@ -49,7 +74,7 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
         // レイアウトファイル list.xml を activity_main.xml に
         // inflate するためにadapterに引数として渡す
         BaseAdapter adapter = new ListViewAdapter(this.getApplicationContext(),
-                R.layout.inflaterlist, scenes, photos);
+                R.layout.inflaterlist, getScenes(), images);
 
         // ListViewにadapterをセット
         listView.setAdapter(adapter);
@@ -69,12 +94,63 @@ public class MainActivity extends Activity implements AdapterView.OnItemClickLis
 
         // clickされたpositionのtextとphotoのID
         String selectedText = scenes[position];
-        int selectedPhoto = photos[position];
+        // int selectedPhoto = images[position];
         // インテントにセット
         intent.putExtra("Text", selectedText);
-        intent.putExtra("Photo", selectedPhoto);
+        //intent.putExtra("Photo", selectedPhoto);
 
         // CharacterIntroductionへ遷移
         startActivity(intent);
+    }
+
+    /**
+     *　キャラクターリスト作成
+     */
+    private void createCharaList() {
+        // JSONを取得
+        String jsonStr = getAssetJsonData();
+
+        JSONObject json = null;
+        JSONArray jsonArr = null;
+        try{
+
+            json = new JSONObject(jsonStr);
+            jsonArr = json.getJSONArray("charaList");
+
+        }catch (org.json.JSONException jex){
+            jex.printStackTrace();
+        }
+        String[] scenes = new String[jsonArr.length()];
+        for(int i = 0 ; i < jsonArr.length() ; i++) {
+            try {
+                JSONObject data = jsonArr.getJSONObject(i);
+
+                // CHARA_NAME
+                scenes[i] = data.getString(CHARA_NAME);
+
+            } catch ( org.json.JSONException ex) {
+                ex.printStackTrace();
+            }
+        }
+        setScenes(scenes);
+    }
+    /**
+     * AssetsのJSONデータ取得処理
+     * @return
+     */
+    private String getAssetJsonData() {
+        String json;
+        try {
+            InputStream is = this.getAssets().open("CharaRoster.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 }
